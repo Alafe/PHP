bced44acd43d02678e6b325eba00ac70807904b0
Alafe <alaferodrigo18@gmail,com>
2020-07-02 22:07:50 -0300

    imprimindo txt

---
+++ imprimindo texto/index.php
@@ -0,0 +1,14 @@
+<?php 
+$nome = "Alafe";
+
+//print retorna 1
+$v = print "Ola, mundo!! ";
+
+//echo não retorna nada
+echo "Ola, Mundo!!<br>";
+
+echo "meu nome é $nome <br>";
+
+echo "meu nome é {$nome} <br>";
+
+echo "meu nome é ".$nome;
